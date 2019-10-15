070ea55f9b1d98f919c8d2065339c17f7a3a60fe
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-10-14 22:54:12 -0400

    Add basic caclulator specs & expectations.

--- MyNotes.md
+++ MyNotes.md
@@ -5,7 +5,12 @@
 - Specs are just tests to be run on the code
 - Specs could be Typescript also
 - The spec file names are based on the target file name e.g. **calculator.spec.js**
-- 
+- Spec => Specifications
+- Specification are like expectations as in a list of things that we expect
+- Expectations are statements whih are assertions e.g. 5 + 5 = 10
+- Assertions can be true or false e.g. 5+5 = 10 -vs- 5+5 = 9
+- If you have a spec with all passing expectations, then it is a passing spec.
+- If one or more expectations fails, then the entire spec fails
 
 ## Suites
 - Suites are a group of tests / specs 
--- Section2-JasmineFunctions/calculator.spec.js
+++ Section2-JasmineFunctions/calculator.spec.js
@@ -1,6 +1,32 @@
-describe("calculator.js Tests", function(){
-	//TODO Add specs
+describe("calculator.js", function(){
 	
+	it("should add() numbers to total", function(){
+		const calc = new Calculator();
+		calc.add(5);
+		//we expect the total to be 5 now
+		expect( calc.total ).toBe(5);
+	});
+	
+	it("should subtract() numbers from the total", function(){
+		const calc = new Calculator();
+		calc.total = 20;
+		calc.subtract( 10 );
+		expect( calc.total ).toBe( 10 );
+	}); 
+
+	it("should multiply() the total by a number", function(){
+		const calc = new Calculator();
+		calc.total = 10;
+		calc.multiply( 2 );
+		expect( calc.total ).toBe( 20 );
+	}); 
+
+	it("should divide() the total by a number", function(){
+		const calc = new Calculator();
+		calc.total = 30;
+		calc.divide( 2 );
+		expect( calc.total ).toBe( 15 );
+	}); 
 	
 	
 	
