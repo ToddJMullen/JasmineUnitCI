b630c18fde7fbf02784c7aa4a70ca83545bc466e
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-10-14 21:17:21 -0400

    Section 2, Calculator v0.1

---
+++ MyNotes.md
@@ -0,0 +1,7 @@
+#Jasmine
+
+## Specs
+
+## Suites
+
+## Expectations
---
+++ Section2-JasmineFunctions/main.js
@@ -0,0 +1,39 @@
+function calculate(inputValue){
+	console.log('calculate/', inputValue );
+	
+	const expression = /[\+\-\/\*]/;
+	const numbers	= inputValue.split(expression);//split based on the expr match
+
+	const numA = +numbers[0];
+	const numB = +numbers[1];
+	const operation = inputValue.match(expression);//get the part of the string that matches the expression
+
+	console.log('calculate/', expression, numbers, operation );
+
+	const c = new Calculator();
+	let result;
+	c.add( numA );
+
+	switch (operation[0]) {
+		case "+": result = c.add(numB); break;
+		case "-": result = c.subtract(numB); break;
+		case "*": result = c.multiply(numB); break;
+		case "/": result = c.divide(numB); break;
+		default:
+			throw new Error(`Unknown operation ${operation}`);
+			break;
+	}
+
+	updateResult(result);
+
+	// debugger;
+}//calculate
+
+function updateResult(result){
+	console.log(`updateResult/`, result );
+	const elem = document.getElementById("result");
+
+	if( elem ){
+		elem.innerText = result;
+	}
+}//updateResult/
