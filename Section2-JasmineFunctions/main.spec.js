3f421872d62c88dba2b6991bcdcf9930df10bb0d
ToddJMullen <ToddJ.Mullen@Gmail.com>
2019-10-19 20:51:06 -0400

    Add comment & example of shared variables using the arrow function vs this

--- Section2-JasmineFunctions/main.spec.js
+++ Section2-JasmineFunctions/main.spec.js
@@ -16,19 +16,58 @@ describe('main.js', function(){
 
 
 	describe("updateResult()", function() {
-		let element;
+		// let element;//converted to this.element to exist throughout
+
+		///////
+		// Warning: Using arrow functions in the 'child' methods changes their context 
+		// and you have to swithc back to the 'shared' parent variable implementation because 
+		// 'this' wont be the correct scope to reference 'element'
+		//////
+
+
 		beforeAll(function(){
+			//run once before all the tests
+			const element = document.createElement("div");
+			element.setAttribute("id", "result"); 
+			document.body.appendChild(element);
+			this.element = element;
+		})
+		afterAll(function(){
+			// const element = document.getElementById("result");//not needed since we already have the handle
+			document.body.removeChild(this.element); 
+		})
+		
+		it("adds result to DOM element", function(){
+			// const element = document.createElement("div");
+			updateResult("5"); 
+			expect( this.element.innerText ).toBe("5");
+
+		})
+		
+	});
+
+	describe("updateResult() with => ", () => {
+		let element;
+
+		///////
+		// Warning: Using arrow functions in the 'child' methods changes their context 
+		// and you have to user the 'shared' parent variable implementation because 
+		// 'this' wont be the correct scope to reference 'element'
+		//////
+
+		beforeAll(() => {
 			//run once before all the tests
 			element = document.createElement("div");
 			element.setAttribute("id", "result"); 
 			document.body.appendChild(element);
+			// element = element;
 		})
-		afterAll(function(){
-			// const element = document.getElementById("result");
+		afterAll(() => {
+			// const element = document.getElementById("result");//not needed since we already have the handle
 			document.body.removeChild(element); 
 		})
 		
-		it("adds result to DOM element", function(){
+		it("adds result to DOM element", () => {
 			// const element = document.createElement("div");
 			updateResult("5"); 
 			expect( element.innerText ).toBe("5");
