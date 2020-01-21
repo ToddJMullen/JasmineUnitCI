a9109788669dd6d698eb075ef43173f3389542ba
ToddJMullen <ToddJ.Mullen@Gmail.com>
2020-01-20 23:03:39 -0500

    Add code coverage reporting Add istanbul, karma-cli, karma-coverage

--- CalculatorApp/karma.conf.js
+++ CalculatorApp/karma.conf.js
@@ -2,16 +2,30 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function(config){
 	config.set({
 		frameworks: ["jasmine","jasmine-matchers"]
+		,preprocessors: {
+			"*.js": ["coverage"]
+		}
 		,files: [//order matters here
 			"./custom-matchers.js"
 			,"*.js"
 			,"*.spec.js"
 		]
-		,plugins: ["karma-jasmine","karma-jasmine-matchers","karma-chrome-launcher"]
-		,reporters: ["dots"]
+		,plugins: [
+			"karma-jasmine"
+			,"karma-jasmine-matchers"
+			,"karma-chrome-launcher"
+			,"karma-coverage"
+		]
+		,reporters: ["dots","coverage"]
 		// ,reporters: ["progress"]
 		,color: true
 		,browsers: ["ChromeHeadless"]
 		,singleRun: true
+		,coverageReporter: {
+			dir: "coverage/"
+			,reporters: [
+				{type: "html", subdir: "html"}
+			]
+		}
 	})
 }
